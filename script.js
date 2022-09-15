502a1e64192c2825e696f9e978bbbe16292ee0b6
kakhi-varazashvili <kakhi_varazashvili@yahoo.it>
2022-09-15 22:46:03 +0400

    rev.01

---
+++ script.js
@@ -0,0 +1,17 @@
+var sixBtns = document.querySelectorAll(".fix-num");
+
+var randomNumbers = document.querySelectorAll(".btn")
+
+
+for(var x of randomNumbers){
+    x.addEventListener("click", function(){
+        this.innerHTML = parseInt(Math.random()*100);
+
+    })
+}
+
+for(var i of sixBtns){
+i.addEventListener("click", function(){
+    this.innerHTML = parseInt(Math.random()*100);
+})
+}
