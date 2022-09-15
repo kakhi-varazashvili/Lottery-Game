e0ca2ed5b1e6e5ee21905e714327abe751641034
kakhi-varazashvili <kakhi_varazashvili@yahoo.it>
2022-09-15 23:06:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ var randomNumbers = document.querySelectorAll(".btn")
 
 for(var x of randomNumbers){
     x.addEventListener("click", function(){
-        this.innerHTML = parseInt(Math.random()*100);
+        this.innerHTML = parseInt(Math.random()*37);
 
     })
 }
 
 for(var i of sixBtns){
 i.addEventListener("click", function(){
-    this.innerHTML = parseInt(Math.random()*100);
+    this.innerHTML = parseInt(Math.random()*37);
 })
 }
